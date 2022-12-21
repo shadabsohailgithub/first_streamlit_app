0a2bc2b1f498abfa638fd2cdec0e8a48ee86ef69
shadabsohailgithub <120662033+shadabsohailgithub@users.noreply.github.com>
2022-12-21 13:29:36 +0530

    Update streamlit_app.py
    Imported Pandas lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🍕🍔🥙Breakfast Menu🍲🍩🍧')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍀Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+
+streamlit.title('🍉🍊🍒🍓Build Your Own Fruit Smoothie🍅🍍🥭🍎')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
